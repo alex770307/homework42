7b8fa292049aab9015f1c74a1367139015dd28b7
alex_ <alex_7377@hotmail.com>
2024-08-01 23:01:38 +0200

    fifth commit

--- src/main/java/userinput/UserInput.java
+++ src/main/java/userinput/UserInput.java
@@ -1,12 +1,18 @@
 package userinput;
 
+import printer.ConsolePrinter;
+import printer.FilePrinter;
+import printer.Printer;
+
 import java.util.Scanner;
 
 public class UserInput {
     private final Scanner scanner;
 
+
     public UserInput(Scanner scanner) {
         this.scanner = scanner;
+
     }
 
     public int getOutputChoice() {
@@ -16,5 +22,21 @@ public class UserInput {
         scanner.nextLine();
 
         return choice;
+    }
+
+    public Printer getPrinter() {
+        int choice = getOutputChoice();
+        Printer printer;
+        if (choice == 1) {
+            printer = new ConsolePrinter();
+        } else if (choice == 2) {
+            System.out.print("Введите имя файла для вывода: ");
+            String filename = scanner.nextLine();
+            printer = new FilePrinter(filename);
+        } else {
+            System.out.println("Неверный выбор, использование консоли по умолчанию.");
+            printer = new ConsolePrinter();
+        }
+        return printer;
     }
 }
